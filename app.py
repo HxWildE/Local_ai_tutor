6f1b1c7eb647f472f2079058f644388b672892b2
= <hb24mcf1r14@student.nitw.ac.in>
2026-02-22 23:17:15 +0530

    Added Prompt and Local Ai : llama38b using Ollama

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
-print("First app") 
+import requests
+
+OLLAMA_URL = "http://localhost:11434/api/generate"
+MODEL = "llama3:8b"
+
+def ask_llm(prompt):
+    payload = {
+        "model": MODEL,
+        "prompt": prompt,
+        "stream": False
+    }
+    response = requests.post(OLLAMA_URL, json=payload)
+    return response.json()["response"]
+
+def chat_loop():
+    print("Local AI Tutor (type 'exit' to quit)\n")
+
+    while True:
+        user_input = input("You: ")
+
+        if user_input.lower() == "exit":
+            print("Bye 👋")
+            break
+
+        reply = ask_llm(user_input)
+        print("\nTutor:", reply, "\n")
+
+if __name__ == "__main__":
+    chat_loop()
+
+    
